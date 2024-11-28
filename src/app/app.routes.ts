7969d7ced330b5497a05b8e1a0dcae219dd4c8ca
YoungDev <jdjoven16@gmail.com>
2024-11-28 15:40:17 -0500

    Nav con rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: CuerpoComponent },
+  { path: 'footer', component: FooterComponent },
+];
